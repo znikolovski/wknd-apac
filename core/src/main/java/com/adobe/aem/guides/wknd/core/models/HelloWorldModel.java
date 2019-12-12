c55f8c72689a418cf1db77d8c39ad034db8451ab
Daniel Gordon <dgordon@adobe.com>
2019-12-12 09:14:58 -0800

    component basics solution

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
@@ -19,6 +19,7 @@ import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_T
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
@@ -48,6 +49,12 @@ public class HelloWorldModel {
     @SlingObject
     private ResourceResolver resourceResolver;
 
+    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
+    protected String greeting;
+    
+    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
+    protected String text;
+    
     private String message;
 
     @PostConstruct
@@ -65,6 +72,14 @@ public class HelloWorldModel {
 
     public String getMessage() {
         return message;
+    }
+    
+    public String getGreeting() {
+    	return StringUtils.isNotBlank(this.greeting) ? this.greeting : "Hello";
+    }
+    
+    public String getTextUpperCase() {
+    	return StringUtils.isNotBlank(this.text) ? this.text.toUpperCase() : null;
     }
 
 }
