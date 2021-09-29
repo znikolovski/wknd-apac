857a1566e76aac95befae7ce99e851d82826e4be
Mark Szulc <mszulc@adobe.com>
2021-09-29 23:14:29 +1000

    update CIF components

--- ui.frontend/src/main/webpack/components/App/App.js
+++ ui.frontend/src/main/webpack/components/App/App.js
@@ -11,103 +11,105 @@
  *    governing permissions and limitations under the License.
  *
  ******************************************************************************/
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { I18nextProvider } from 'react-i18next';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import {
-    CommerceApp,
-    Portal,
-    ConfigContextProvider,
-    Cart,
-    CartTrigger,
-    AuthBar,
-    AccountContainer,
-    AddressBook,
-    BundleProductOptions,
-    AccountDetails,
-    ResetPassword,
-    PortalPlacer
-} from '@adobe/aem-core-cif-react-components';
-
-import {
-    ProductRecsGallery,
-    StorefrontInstanceContextProvider
-} from '@adobe/aem-core-cif-product-recs-extension';
-
-import i18n from './i18n';
-import partialConfig from './config';
-
-import '../../site/main.scss';
-
-const App = () => {
-    const { storeView, graphqlEndpoint, graphqlMethod, httpHeaders } = document.querySelector('body').dataset;
-    const { mountingPoints, pagePaths } = partialConfig;
-
-    console.log(httpHeaders);
-
-    const config = {
-        ...partialConfig,
-        storeView,
-        graphqlEndpoint,
-        // Can be GET or POST. When selecting GET, this applies to cache-able GraphQL query requests only. Mutations
-        // will always be executed as POST requests.
-        graphqlMethod,
-        headers: JSON.parse(httpHeaders)
-    };
-
-    return (
-        <I18nextProvider i18n={i18n} defaultNS="common">
-            <ConfigContextProvider config={config}>
-                <CommerceApp>
-                    <StorefrontInstanceContextProvider>
-                        <PortalPlacer selector={'[data-is-product-recs]'} component={ProductRecsGallery} />
-                    </StorefrontInstanceContextProvider>
-                    <Portal selector={mountingPoints.cartTrigger}>
-                        <CartTrigger />
-                    </Portal>
-                    <Portal selector={mountingPoints.minicart}>
-                        <Cart />
-                    </Portal>
-                    <Portal selector={mountingPoints.authBarContainer}>
-                        <AuthBar />
-                    </Portal>
-                    <Portal selector={mountingPoints.accountContainer}>
-                        <AccountContainer />
-                    </Portal>
-                    <Route path={pagePaths.addressBook}>
-                        <Portal selector={mountingPoints.addressBookContainer}>
-                            <AddressBook />
-                        </Portal>
-                    </Route>
-                    <Route path={pagePaths.resetPassword}>
-                        <Portal selector={mountingPoints.resetPasswordPage}>
-                            <ResetPassword />
-                        </Portal>
-                    </Route>
-                    <Portal selector={mountingPoints.bundleProductOptionsContainer}>
-                        <BundleProductOptions />
-                    </Portal>
-                    <Route path={pagePaths.accountDetails}>
-                        <Portal selector={mountingPoints.accountDetails}>
-                            <AccountDetails />
-                        </Portal>
-                    </Route>
-                </CommerceApp>
-            </ConfigContextProvider>
-        </I18nextProvider>
-    );
-};
-
-window.onload = () => {
-    const root = document.createElement('div');
-    document.body.appendChild(root);
-    ReactDOM.render(
-        <Router>
-            <App />
-        </Router>,
-        root
-    );
-};
-
-export default App;
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+ import { IntlProvider } from 'react-intl';
+ import { BrowserRouter as Router, Route } from 'react-router-dom';
+ import {
+     CommerceApp,
+     Portal,
+     ConfigContextProvider,
+     Cart,
+     CartTrigger,
+     AuthBar,
+     AccountContainer,
+     AddressBook,
+     BundleProductOptions,
+     AccountDetails,
+     ResetPassword,
+     PortalPlacer
+ } from '@adobe/aem-core-cif-react-components';
+ 
+ import {
+     ProductRecsGallery,
+     StorefrontInstanceContextProvider
+ } from '@adobe/aem-core-cif-product-recs-extension';
+ 
+ import loadLocaleData from './i18n';
+ import partialConfig from './config';
+ 
+ import '../../site/main.scss';
+ 
+ const App = props => {
+     const { storeView, graphqlEndpoint, graphqlMethod, httpHeaders } = document.querySelector('body').dataset;
+     const { mountingPoints, pagePaths } = partialConfig;
+     const { locale, messages } = props;
+ 
+     const config = {
+         ...partialConfig,
+         storeView,
+         graphqlEndpoint,
+         // Can be GET or POST. When selecting GET, this applies to cache-able GraphQL query requests only. Mutations
+         // will always be executed as POST requests.
+         graphqlMethod,
+         headers: JSON.parse(httpHeaders)
+     };
+ 
+     return (
+         <IntlProvider locale={locale} messages={messages}>
+             <ConfigContextProvider config={config}>
+                 <CommerceApp>
+                     <StorefrontInstanceContextProvider>
+                         <PortalPlacer selector={'[data-is-product-recs]'} component={ProductRecsGallery} />
+                     </StorefrontInstanceContextProvider>
+                     <Portal selector={mountingPoints.cartTrigger}>
+                         <CartTrigger />
+                     </Portal>
+                     <Portal selector={mountingPoints.minicart}>
+                         <Cart />
+                     </Portal>
+                     <Portal selector={mountingPoints.authBarContainer}>
+                         <AuthBar />
+                     </Portal>
+                     <Portal selector={mountingPoints.accountContainer}>
+                         <AccountContainer />
+                     </Portal>
+                     <Route path={pagePaths.addressBook}>
+                         <Portal selector={mountingPoints.addressBookContainer}>
+                             <AddressBook />
+                         </Portal>
+                     </Route>
+                     <Route path={pagePaths.resetPassword}>
+                         <Portal selector={mountingPoints.resetPasswordPage}>
+                             <ResetPassword />
+                         </Portal>
+                     </Route>
+                     <Portal selector={mountingPoints.bundleProductOptionsContainer}>
+                         <BundleProductOptions />
+                     </Portal>
+                     <Route path={pagePaths.accountDetails}>
+                         <Portal selector={mountingPoints.accountDetails}>
+                             <AccountDetails />
+                         </Portal>
+                     </Route>
+                 </CommerceApp>
+             </ConfigContextProvider>
+         </IntlProvider>
+     );
+ };
+ 
+ window.onload = async () => {
+     const { locale, messages } = await loadLocaleData();
+     const root = document.createElement('div');
+     
+     document.body.appendChild(root);
+ 
+     ReactDOM.render(
+         <Router>
+             <App locale={locale} messages={messages} />
+         </Router>,
+         root
+     );
+ };
+ 
+ export default App;
